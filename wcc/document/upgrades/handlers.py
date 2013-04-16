5f0629488c6503a198380e081d6cd718182cc3b9
Izhar Firdaus <izhar@inigo-tech.com>
2013-04-17 04:55:29 +0800

    allow excludefromnavigation on document content type

--- wcc/document/upgrades/handlers.py
+++ wcc/document/upgrades/handlers.py
@@ -4,6 +4,24 @@ from Products.CMFCore.utils import getToolByName
 # -*- extra stuff goes here -*- 
 
 
+@gs.upgradestep(title=u'Upgrade wcc.document to 1003',
+                description=u'Upgrade wcc.document to 1003',
+                source='1002', destination='1003',
+                sortkey=1, profile='wcc.document:default')
+def to1003(context):
+    setup = getToolByName(context, 'portal_setup')
+    setup.runAllImportStepsFromProfile('profile-wcc.document.upgrades:to1003')
+
+    catalog = getToolByName(context, 'portal_catalog')
+
+    for brain in catalog(portal_type=['wcc.churches.churchmember',
+                                    'wcc.churches.churchfamily',
+                                    'wcc.churches.churchbody'],
+                                    Language='all'):
+        obj = brain.getObject()
+        obj.reindexObject()
+
+
 @gs.upgradestep(title=u'Upgrade wcc.document to 1002',
                 description=u'Upgrade wcc.document to 1002',
                 source='1', destination='1002',
